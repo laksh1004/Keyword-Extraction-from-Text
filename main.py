bc366343274f229a3f7c69f164f443f90618cea8
Lakshay Kumar <35694363+laksh1004@users.noreply.github.com>
2018-07-15 00:55:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,7 @@ filename = 'text.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
-# split into words by white space
 
-text = re.sub('[^a-zA-Z]', ' ', str(text))
-text = text.lower()
-# =============================================================================
-# text = text.split()
-# ps = PorterStemmer()
-# text = [ps.stem(word) for word in text if not word in set(stopwords.words('english'))]
-# text = ' '.join(text)
-# 
-# =============================================================================
 r = Rake()
 r.extract_keywords_from_text(text)
 r.get_ranked_phrases()
